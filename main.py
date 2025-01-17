1d5d49d7bd1e66f7cf3991fa4ee39999fef27fc3
Matthieu <matthieu@mbpdematthieu2-1.home>
2025-01-17 16:30:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,18 @@ def main(refence, test, crop_size):
         print(f'Images come from the same source')
     else:
         print(f'Images come from a different source')
+
+    # Create outputs
+    if not os.path.exists('outputs'):
+        os.mkdir('outputs')
+
+    # Display histograms
+    # Merged
+    plt.hist(list_corr, bins='auto', alpha=0.7, edgecolor='black', label=r"$\rho(P^r_i, P^p_j):i \neq j$")
+    plt.hist(list_corr_ij, bins='auto', alpha=0.7, edgecolor='black', label=r"$\rho(P^r_i, P^p_j):x_i=x_j$")
+    plt.title(f'p_value = {p_value:.2e}')
+    plt.legend(loc='upper right')
+    plt.savefig('./outputs/histo.png')
     exit(0)
 
 
